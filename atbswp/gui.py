573ce2172dce23364877d456113e62c6eca2f205
RMPR <akoudanilo@gmail.com>
2020-02-25 19:54:20 +0100

    Create a taskbar icon for the application

--- atbswp/gui.py
+++ atbswp/gui.py
@@ -27,6 +27,7 @@ from pathlib import Path
 import control
 
 import wx
+import wx.adv
 
 
 class MainDialog(wx.Dialog, wx.MiniFrame):
@@ -184,3 +185,57 @@ class MainDialog(wx.Dialog, wx.MiniFrame):
             self.OnExitApp(event)
         else:
             event.StopPropagation()
+
+
+TRAY_TOOLTIP = 'Name'
+TRAY_ICON = 'icon.png'
+
+def create_menu_item(menu, label, func):
+    item = wx.MenuItem(menu, -1, label)
+    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+    menu.Append(item)
+    return item
+
+
+class TaskBarIcon(wx.adv.TaskBarIcon):
+    def __init__(self, frame):
+        self.frame = frame
+        super(TaskBarIcon, self).__init__()
+        self.set_icon(TRAY_ICON)
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        create_menu_item(menu, 'Site', self.on_hello)
+        menu.AppendSeparator()
+        create_menu_item(menu, 'Exit', self.on_exit)
+        return menu
+
+    def set_icon(self, path):
+        icon = wx.Icon(path)
+        self.SetIcon(icon, TRAY_TOOLTIP)
+
+    def on_left_down(self, event):
+        print ('Tray icon was left-clicked.')
+
+    def on_hello(self, event):
+        print ('Hello, world!')
+
+    def on_exit(self, event):
+        wx.CallAfter(self.Destroy)
+        self.frame.Close()
+
+class App(wx.App):
+    def OnInit(self):
+        frame=wx.Frame(None)
+        self.SetTopWindow(frame)
+        TaskBarIcon(frame)
+        return True
+
+def main():
+    app = App(False)
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
