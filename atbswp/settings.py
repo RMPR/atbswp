a9c381127995b45c8e439f86395675e5b6056c2f
Justin Ting <jting@atlassian.com>
2020-06-28 23:21:43 +1000

    string-literal-fix - change en to 'en' to prevent error

--- atbswp/settings.py
+++ atbswp/settings.py
@@ -51,4 +51,4 @@ except OSError:
                          'Recording Hotkey': 348,
                          'Playback Hotkey': 349,
                          'Always On Top': True,
-                         'Language': en}
+                         'Language': 'en'}
